21861ccfb38648b02a0598af1fa5ba0151457c78
Guilherme Kramer <guilherme_ok@hotmail.com>
2026-01-22 16:33:21 -0300

    feat new types for handle the requests

---
+++ frontend/src/types/types.ts
@@ -0,0 +1,36 @@
+export interface CharacterOrigin {
+  name: string;
+  url: string;
+}
+
+export interface CharacterLocation {
+  name: string;
+  url: string;
+}
+
+export interface Character {
+  id: number;
+  name: string;
+  status: 'Alive' | 'Dead' | 'unknown';
+  species: string;
+  type: string;
+  gender: 'Female' | 'Male' | 'Genderless' | 'unknown';
+  origin: CharacterOrigin;
+  location: CharacterLocation;
+  image: string;
+  episode: string[];
+  url: string;
+  created: string;
+}
+
+export interface PaginationInfo {
+  count: number;
+  pages: number;
+  next: string | null;
+  prev: string | null;
+}
+
+export interface CharactersResponse {
+  info: PaginationInfo;
+  results: Character[];
+}
