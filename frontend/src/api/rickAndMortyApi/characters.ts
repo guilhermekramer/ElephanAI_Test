e272ff16894d1a280bd0cdbe44c86736109f9860
Guilherme Kramer <guilherme_ok@hotmail.com>
2026-01-22 19:04:44 -0300

    fix: deleted the useless function for get chatacteres, add the name param to the getall characteres

--- frontend/src/api/rickAndMortyApi/characters.ts
+++ frontend/src/api/rickAndMortyApi/characters.ts
@@ -1,12 +1,7 @@
 import rickAndMortyApi from '../rickAndMortyApi/index';
 import type { Character, CharactersResponse } from './../../types/types';
 
-export async function getCharacters( page: number = 1 ): Promise<CharactersResponse> {
-  const { data } = await rickAndMortyApi.get<CharactersResponse>( '/character', { params: { page }, })
-  return data
-}
-
-export async function getCharacterById( id: number ): Promise<Character> {
-  const { data } = await rickAndMortyApi.get<Character>( `/character/${id}`)
+export async function getCharacters( page: number = 1, name?: string ): Promise<CharactersResponse> {
+  const { data } = await rickAndMortyApi.get<CharactersResponse>( '/character', { params: { page, name: name || undefined }, })
   return data
 }
