2b5ac028e6a13060bbe5a3eeffa5cb65fa19826d
Guilherme Kramer <guilherme_ok@hotmail.com>
2026-01-23 12:20:23 -0300

    fix: function addapted to recieve array type

--- frontend/src/api/rickAndMortyApi/characters.ts
+++ frontend/src/api/rickAndMortyApi/characters.ts
@@ -1,7 +1,15 @@
 import rickAndMortyApi from '../rickAndMortyApi/index';
-import type { CharactersResponse } from './../../types/types';
+import type { CharactersResponse, Character } from './../../types/types';
 
 export async function getCharacters( page: number = 1, name?: string ): Promise<CharactersResponse> {
   const { data } = await rickAndMortyApi.get<CharactersResponse>( '/character', { params: { page, name: name || undefined }, })
   return data
+}
+
+export async function getCharactersByIds(ids: number[]): Promise<Character[]> {
+  if (ids.length === 0) return []
+  const { data } = await rickAndMortyApi.get<Character | Character[]>(
+    `/character/${ids.join(',')}`
+  )
+  return Array.isArray(data) ? data : [data]
 }
